4e88cec7d949c9b9e040ebed5be2cbedb5b0ea21
marco candrian <mac@calmar.ws>
2011-06-15 09:10:37 +0200

    splitting up read_line

--- remote_akkumatik.py
+++ remote_akkumatik.py
@@ -36,6 +36,163 @@ def output_data(output, label, output2, label2): #{{{
         gtk.main_iteration()
 
 #}}}
+
+def generate_output_strs(daten): #{{{
+    """Create the strings for the Display (labels) """
+
+    ausgang = str(int(daten[0][-1:])) #Ausgang
+    zeit = daten[1] #Stunden Minuten Sekunden
+    lade_v = int(daten[2])/1000.0 #Akkuspannung mV
+    lade_v = "%6.3fV" % (lade_v) #format into string
+    ampere = int(daten[3]) #Strom A
+    if ampere >= 1000 or ampere <= -1000:
+        ampere = "%+.2fA" % (ampere/1000.0)
+    else:
+        ampere = "%imA" % (ampere)
+
+    amph = int(daten[4])/1000.0 #Ladungsmenge amph
+    vers_u = int(daten[5])/1000.0 #Versorungsspannung mV
+    rimohm_baldelta = int(daten[6]) #akku-unnen mOhm
+    c_bat = int(daten[7]) #Akkutemperatur
+    tmp_zellen = int(daten[8]) #Zellenzahl / bei Stop -> 'Fehlercode'
+    if tmp_zellen < 50:
+        cfg.ANZAHL_ZELLEN[int(ausgang)] = tmp_zellen
+
+    phase = int(daten[9]) #Ladephase 0-stop ...
+    if phase == 0:
+        cfg.BUTTON_START.set_sensitive(True)
+        cfg.BUTTON_STOP.set_sensitive(False)
+    else:
+        if phase == 10: #Pause
+            cfg.BUTTON_START.set_sensitive(True)
+        else:
+            cfg.BUTTON_START.set_sensitive(False)
+        cfg.BUTTON_STOP.set_sensitive(True)
+
+    #TODO 'beim Formieren' also sonst immer 0? dann output2 anpassen
+    zyklus = int(daten[10]) #Zyklus
+    #sp = int(daten[11]) #Aktive Akkuspeicher
+
+    cfg.ATYP[cfg.GEWAEHLTER_AUSGANG] = int(daten[12]) #Akkutyp
+    atyp_str = cfg.AKKU_TYP[int(daten[12])] #Akkutyp
+
+    cfg.PRG[cfg.GEWAEHLTER_AUSGANG] = int(daten[13]) #Programm
+    prg_str = cfg.AMPROGRAMM[int(daten[13])] #Programm
+
+    try:
+        cfg.LART[cfg.GEWAEHLTER_AUSGANG] = int(daten[14]) #Ladeart
+        lart_str = cfg.LADEART[int(daten[14])] #Ladeart
+    except IndexError, err:
+        print "%s" % err
+        print "-> %i" % int(daten[14])
+        time.sleep(10)
+        sys.exit()
+
+    cfg.STROMW[cfg.GEWAEHLTER_AUSGANG] = int(daten[15]) #stromwahl
+    stromw_str = cfg.STROMWAHL[int(daten[15])] #stromwahl
+
+    cfg.STOPPM[cfg.GEWAEHLTER_AUSGANG] = int(daten[16]) #stromwahl
+    stoppm_str = cfg.STOPPMETHODE[int(daten[16])] #stromwahl
+
+    c_kk = int(daten[17]) #KK Celsius
+
+    tmp_a = []
+    for cell in daten[18:-1]:
+        try:
+            tmp_a.append(int(cell))
+        except IndexError:
+            print "00:00:00 to long error"
+            print daten
+            print "----------------------"
+
+    balance_delta = -1
+    if len(tmp_a) > 0:
+        balance_delta = max(tmp_a) - min(tmp_a)
+
+    if phase == 0: #dann 'Fehlercode' zwangsweise ...?
+        if tmp_zellen >= 54: # FEHLER
+            output = cfg.FEHLERCODE[tmp_zellen - 50]
+            output_data(output, labels[0], "", labels[1])
+            return True
+
+        if tmp_zellen >= 50: #'gute' codes
+            phasedesc = "%-11s" % (cfg.FEHLERCODE[tmp_zellen - 50])
+            ausgang = ""
+            lade_v = ""
+
+        else:
+            phasedesc = "?????" # should never happen possibly
+
+    # 51 VOLL   Ladevorgang wurde korrekt beendet, Akku ist voll geladen
+    # 52 LEER   Entladevorgang wurde korrekt beendet, Akku ist leer
+    # TODO: 52 od 51 + "x..." FERTIG Lipo-Lagerprogramm wurde korrekt 
+    #         beendet, Akku ist fertig zum Lagern
+    # TODO: 52 od 51 + "x ": ? MENGE  Vorgang wurde durch eingestelltes 
+    #        Mengenlimit beendet
+    # 50 STOP Vorgang wurde manuell (vorzeitig) beendet
+    # FEHLER Vorgang wurde fehlerhaft beendet
+
+    elif phase == 10:
+        phasedesc = " PAUSE    "
+        lade_v = ""
+    else:
+        if phase >= 1 and phase <= 5:
+            tmp = "L"
+        elif phase >= 7 and phase <= 9:
+            tmp = "E"
+            phase = "-"
+
+        phasedesc = atyp_str[0:1] + tmp + str(phase)
+
+    #< stunde dann Minunte:Sekunden, sonst Stunde:Minuten
+    if zeit[0:2] == "00":
+        zeit = zeit [3:]
+    else:
+        zeit = zeit [:-3]
+
+    #label print
+    rimohm_baldelta = "Ri:%03i" % (rimohm_baldelta)
+    #TODO:  more elgegant...
+    if cfg.ATYP[cfg.GEWAEHLTER_AUSGANG] == 5: #LiPo
+        if len(daten) > 19:
+            rimohm_baldelta = "∆%2imV " % (balance_delta)
+        else:
+            rimohm_baldelta = "∆..mV "
+        cfg.MENGE[cfg.GEWAEHLTER_AUSGANG] = 0
+        lart_str = "[LiPo]"
+        stoppm_str = "[LiPo]"
+
+    output ="%s%s %s %s\n%-7s   %+6.3fAh" % (ausgang, phasedesc, lade_v, \
+            zeit, ampere, amph)
+
+    zykll = str(cfg.ZYKLEN[cfg.GEWAEHLTER_AUSGANG])
+    if zykll == "0":
+        zykll = "-"
+
+    output2 ="%ix%s %2i° %s Z:%1i/%s\n" % \
+            (cfg.ANZAHL_ZELLEN[cfg.GEWAEHLTER_AUSGANG], atyp_str, c_bat,\
+            rimohm_baldelta, zyklus, zykll)
+    output2 +="%s %s %s %s\n" % (prg_str, lart_str, stromw_str, stoppm_str)
+
+    kapa = str(cfg.KAPAZITAET[cfg.GEWAEHLTER_AUSGANG])
+    llimit = str(cfg.LADELIMIT[cfg.GEWAEHLTER_AUSGANG])
+    entll = str( cfg.ENTLADELIMIT[cfg.GEWAEHLTER_AUSGANG])
+    menge_str = str(cfg.MENGE[cfg.GEWAEHLTER_AUSGANG])
+    if kapa == "0":
+        kapa = "-"
+    if llimit == "0":
+        llimit = "-"
+    if entll == "0":
+        entll = "-"
+    if menge_str == "0":
+        menge_str = "-"
+
+    output2 +="Kap:%smAh ILa:%smA IEn:%smA\n" % (kapa , llimit, entll)
+    output2 +="Menge:%smAh VerU:%5.2fV %2i°KK\n" % (menge_str, vers_u, c_kk)
+
+    return (output, output2)
+#}}}
+
 def read_line(labels): #{{{
     """Read serial data (called via interval via 
     gobject.timeout_add) and print it to display"""
@@ -90,160 +247,10 @@ def read_line(labels): #{{{
         cfg.FSER = helper.open_file(cfg.TMP_DIR+'/serial-akkumatik.dat', 'ab')
         return True
 
-
     if (daten[0] == "1" and cfg.GEWAEHLTER_AUSGANG == 1) \
             or (daten[0] == "2" and cfg.GEWAEHLTER_AUSGANG == 2):
-        ausgang = str(int(daten[0][-1:])) #Ausgang
-        zeit = daten[1] #Stunden Minuten Sekunden
-        lade_v = int(daten[2])/1000.0 #Akkuspannung mV
-        lade_v = "%6.3fV" % (lade_v) #format into string
-        ampere = int(daten[3]) #Strom A
-        if ampere >= 1000 or ampere <= -1000:
-            ampere = "%+.2fA" % (ampere/1000.0)
-        else:
-            ampere = "%imA" % (ampere)
-
-        amph = int(daten[4])/1000.0 #Ladungsmenge amph
-        vers_u = int(daten[5])/1000.0 #Versorungsspannung mV
-        rimohm_baldelta = int(daten[6]) #akku-unnen mOhm
-        c_bat = int(daten[7]) #Akkutemperatur
-        tmp_zellen = int(daten[8]) #Zellenzahl / bei Stop -> 'Fehlercode'
-        if tmp_zellen < 50:
-            cfg.ANZAHL_ZELLEN[int(ausgang)] = tmp_zellen
-
-        phase = int(daten[9]) #Ladephase 0-stop ...
-        if phase == 0:
-            cfg.BUTTON_START.set_sensitive(True)
-            cfg.BUTTON_STOP.set_sensitive(False)
-        else:
-            if phase == 10: #Pause
-                cfg.BUTTON_START.set_sensitive(True)
-            else:
-                cfg.BUTTON_START.set_sensitive(False)
-            cfg.BUTTON_STOP.set_sensitive(True)
-
-        #TODO 'beim Formieren' also sonst immer 0? dann output2 anpassen
-        zyklus = int(daten[10]) #Zyklus
-        #sp = int(daten[11]) #Aktive Akkuspeicher
-
-        cfg.ATYP[cfg.GEWAEHLTER_AUSGANG] = int(daten[12]) #Akkutyp
-        atyp_str = cfg.AKKU_TYP[int(daten[12])] #Akkutyp
-
-        cfg.PRG[cfg.GEWAEHLTER_AUSGANG] = int(daten[13]) #Programm
-        prg_str = cfg.AMPROGRAMM[int(daten[13])] #Programm
-
-        try:
-            cfg.LART[cfg.GEWAEHLTER_AUSGANG] = int(daten[14]) #Ladeart
-            lart_str = cfg.LADEART[int(daten[14])] #Ladeart
-        except IndexError, err:
-            print "%s" % err
-            print "-> %i" % int(daten[14])
-            time.sleep(10)
-            sys.exit()
-
-        cfg.STROMW[cfg.GEWAEHLTER_AUSGANG] = int(daten[15]) #stromwahl
-        stromw_str = cfg.STROMWAHL[int(daten[15])] #stromwahl
-
-        cfg.STOPPM[cfg.GEWAEHLTER_AUSGANG] = int(daten[16]) #stromwahl
-        stoppm_str = cfg.STOPPMETHODE[int(daten[16])] #stromwahl
-
-        c_kk = int(daten[17]) #KK Celsius
-
-        tmp_a = []
-        for cell in daten[18:-1]:
-            try:
-                tmp_a.append(int(cell))
-            except IndexError:
-                print "00:00:00 to long error"
-                print daten
-                print "----------------------"
-
-        balance_delta = -1
-        if len(tmp_a) > 0:
-            balance_delta = max(tmp_a) - min(tmp_a)
-
-        if phase == 0: #dann 'Fehlercode' zwangsweise ...?
-            if tmp_zellen >= 54: # FEHLER
-                output = cfg.FEHLERCODE[tmp_zellen - 50]
-                output_data(output, labels[0], "", labels[1])
-                return True
-
-            if tmp_zellen >= 50: #'gute' codes
-                phasedesc = "%-11s" % (cfg.FEHLERCODE[tmp_zellen - 50])
-                ausgang = ""
-                lade_v = ""
-
-            else:
-                phasedesc = "?????" # should never happen possibly
-
-        # 51 VOLL   Ladevorgang wurde korrekt beendet, Akku ist voll geladen
-        # 52 LEER   Entladevorgang wurde korrekt beendet, Akku ist leer
-        # TODO: 52 od 51 + "x..." FERTIG Lipo-Lagerprogramm wurde korrekt 
-        #         beendet, Akku ist fertig zum Lagern
-        # TODO: 52 od 51 + "x ": ? MENGE  Vorgang wurde durch eingestelltes 
-        #        Mengenlimit beendet
-        # 50 STOP Vorgang wurde manuell (vorzeitig) beendet
-        # FEHLER Vorgang wurde fehlerhaft beendet
-
-        elif phase == 10:
-            phasedesc = " PAUSE    "
-            lade_v = ""
-        else:
-            if phase >= 1 and phase <= 5:
-                tmp = "L"
-            elif phase >= 7 and phase <= 9:
-                tmp = "E"
-                phase = "-"
-
-            phasedesc = atyp_str[0:1] + tmp + str(phase)
-
-        #< stunde dann Minunte:Sekunden, sonst Stunde:Minuten
-        if zeit[0:2] == "00":
-            zeit = zeit [3:]
-        else:
-            zeit = zeit [:-3]
-
-        #label print
-        rimohm_baldelta = "Ri:%03i" % (rimohm_baldelta)
-        #TODO:  more elgegant...
-        if cfg.ATYP[cfg.GEWAEHLTER_AUSGANG] == 5: #LiPo
-            if len(daten) > 19:
-                rimohm_baldelta = "∆%2imV " % (balance_delta)
-            else:
-                rimohm_baldelta = "∆..mV "
-            cfg.MENGE[cfg.GEWAEHLTER_AUSGANG] = 0
-            lart_str = "[LiPo]"
-            stoppm_str = "[LiPo]"
-
-        output ="%s%s %s %s\n%-7s   %+6.3fAh" % (ausgang, phasedesc, lade_v, \
-                zeit, ampere, amph)
-
-        zykll = str(cfg.ZYKLEN[cfg.GEWAEHLTER_AUSGANG])
-        if zykll == "0":
-            zykll = "-"
-
-        output2 ="%ix%s %2i° %s Z:%1i/%s\n" % \
-                (cfg.ANZAHL_ZELLEN[cfg.GEWAEHLTER_AUSGANG], atyp_str, c_bat,\
-                rimohm_baldelta, zyklus, zykll)
-        output2 +="%s %s %s %s\n" % (prg_str, lart_str, stromw_str, stoppm_str)
-
-        kapa = str(cfg.KAPAZITAET[cfg.GEWAEHLTER_AUSGANG])
-        llimit = str(cfg.LADELIMIT[cfg.GEWAEHLTER_AUSGANG])
-        entll = str( cfg.ENTLADELIMIT[cfg.GEWAEHLTER_AUSGANG])
-        menge_str = str(cfg.MENGE[cfg.GEWAEHLTER_AUSGANG])
-        if kapa == "0":
-            kapa = "-"
-        if llimit == "0":
-            llimit = "-"
-        if entll == "0":
-            entll = "-"
-        if menge_str == "0":
-            menge_str = "-"
-
-        output2 +="Kap:%smAh ILa:%smA IEn:%smA\n" % (kapa , llimit, entll)
-        output2 +="Menge:%smAh VerU:%5.2fV %2i°KK\n" % (menge_str, vers_u, c_kk)
-
-        output_data(output, labels[0], output2, labels[1])
+        (labstr1, labstr2) = generate_output_strs(daten)
+        output_data(labstr1, labels[0], labstr2, labels[1])
 
     return True
 
